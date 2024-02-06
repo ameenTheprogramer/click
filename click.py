475c0822edc490d2b298738ec0c07040a0108e20
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-06 14:58:44 +0530

    Create click.py

---
+++ click.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui
+
+def click_at_coordinates(x, y):
+    # Wait for 10 seconds
+    time.sleep(10)
+    # Move the mouse to the specified coordinates and click
+    pyautogui.click(x, y)
+
+# Call the function with the desired coordinates
+click_at_coordinates(743, 554)
