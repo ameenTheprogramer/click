e877b24b99fd662eb141e31931b640fd18eeb4e6
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-06 15:18:23 +0530

    Update scroll.py

--- scroll.py
+++ scroll.py
@@ -1,10 +1,10 @@
-import pyautogui
 import time
+import pyautogui
 
 def scroll():
     time.sleep(10)
     
-    pyautogui.scroll(100)
+    pyautogui.scroll(-10)
 
 if __name__ == "__main__":
     scroll()
